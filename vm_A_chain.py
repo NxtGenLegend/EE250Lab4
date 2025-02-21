c400ec80b73bc888534ec2dd6974b5435a121bbe
Adhish Chakravorty <skjetly094@gmail.com>
2025-02-20 17:17:14 -0800

    Update vm_A_chain.py

--- vm_A_chain.py
+++ vm_A_chain.py
@@ -3,7 +3,15 @@ import time
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to MQTT Broker")
+    client.subscribe("adhishch/pong")
     client.publish("adhishch/ping", "1")
+    client.message_callback_add("adhishch/ping", on_message_ping)
+
+def on_message_ping(client, userdata, msg):
+    number = int(msg.payload.decode()) + 1
+    print(f"Received: {msg.payload.decode()}, Sending: {number}")
+    time.sleep(1)
+    client.publish("adhishch/ping", str(number))
 
 if __name__ == '__main__':
     client = mqtt.Client()
